10135ca7831f0811cee1e94fcb3239636a5ad9a2
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-05-26 21:43:16 +0900

    Feat: 제네릭 기반 UIManager 및 UIBase 구조 구현
    - 제네릭 기반 UIManager 및 UIBase 구조 구현

---
+++ Assets/01.Scripts/02.UI/GameScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            UIManager.Instance.Open<MainUI>();
+
+        if (Input.GetKeyDown(KeyCode.E))
+            UIManager.Instance.Close<MainUI>();
+    }
+}
