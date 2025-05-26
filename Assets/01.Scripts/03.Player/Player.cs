3f0483d4e222bcf1affe2523a10a9b697b69de8e
garamzui <forsave121@gmail.com>
2025-05-26 18:57:35 +0900

    Feat : 양 옆 이동 구현
    - 양 옆의 포지션 변경으로 사이드 스탭을 구현

--- Assets/01.Scripts/03.Player/Player.cs
+++ Assets/01.Scripts/03.Player/Player.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public abstract class Player : MonoBehaviour
 {
-    protected Rigidbody rb;
+   protected Rigidbody rb;
+   protected Transform tf;
     protected Animator anim;
-    protected Transform tf;
+   
     [Header("이동 관련 스탯")]
-    [SerializeField]private float runSpeed;
+    [SerializeField]protected float runSpeed;
     public float RunSpeed{get{return runSpeed;}}
-    [SerializeField] private float jumpForce;
+    [SerializeField] protected float jumpForce;
     public float JumpForce{get{return jumpForce;}}  
-    [SerializeField]
-    private float sideStepDistance;
-    
-    void Awake()
+    [SerializeField] protected float sideStepDistance;
+    [SerializeField]protected float maxDistance;
+    void Start()
     {
-     rb = GetComponent<Rigidbody>();
+     
      anim = GetComponent<Animator>();
      tf = GetComponent<Transform>();
+     rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
--- Assets/01.Scripts/03.Player/PlayerController.cs
+++ Assets/01.Scripts/03.Player/PlayerController.cs
@@ -5,67 +5,88 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : Player
 {
-    
+    void Awake()
+    {
+    }
+
+
     void Update()
     {
-        
     }
 
     void FixedUpdate()
     {
         Run();
     }
- void Run()
+
+    void Run()
     {
+        if (rb == null)
+        {
+            return;
+        }
+
         Vector3 run = rb.velocity;
-       
-        
+
+
         run.z = RunSpeed;
-        
-         rb.velocity = run;
+
+        rb.velocity = run;
     }
-    void OnSideStep (InputAction.CallbackContext context)
+
+    public void OnSideStep(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
+            if (context.control.name == "leftArrow")
+            {
+                if (tf.position.x == -maxDistance)
+                {
+                    return;
+                }
+
+                tf.position -= new Vector3(sideStepDistance, 0, 0);
+            }
+            else if (context.control.name == "rightArrow")
+            {
+                if (tf.position.x == maxDistance)
+                {
+                    return;
+                }
+
+                tf.position += new Vector3(sideStepDistance, 0, 0);
+            }
+        }
+        else 
+        {
+            return;
         }
     }
 
     void SideStep()
     {
-        
     }
 
-   
 
     void OnJump(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
-            
         }
     }
 
     void Jump()
     {
-        
     }
 
     void OnCrouch(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
-            
         }
     }
 
     void Crouch()
     {
-        
     }
-
-
-
-
-
 }
