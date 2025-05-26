0d762144fe1ce9dd1e88ca002add96e18a8451b0
garamzui <forsave121@gmail.com>
2025-05-26 14:43:19 +0900

    init
    매니저 싱글톤 작업

---
+++ Assets/01.Scripts/01.Managers/GameManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; }
+    }
+    public SoundManager soundManager;
+    public UIManager uiManager;
+    private Player player;
+       public Player Player{get{return player;}}
+    private void Awake()
+    {
+        if(instance ==  null)
+            {
+            instance = this;   
+            DontDestroyOnLoad(gameObject);
+            }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Start()
+    {
+        player=FindObjectOfType<Player>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/01.Scripts/03.Player/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : Player
+{
+    
+    void Start()
+    {
+        
+    }
+    
+   
+    void Update()
+    {
+        
+    }
+}
