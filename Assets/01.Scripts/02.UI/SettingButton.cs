b2907a4a926719d079403ffbe8f710f15c18fc30
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-27 13:03:34 +0900

    Feat : SettingButton 프리팹화
    스크립트 Start, Update 삭제

--- Assets/01.Scripts/02.UI/SettingButton.cs
+++ Assets/01.Scripts/02.UI/SettingButton.cs
@@ -2,18 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingButton : MonoBehaviour
+public class SettingButton : UIBase
 {
-    // Start is called before the first frame update
-    // 주석 안깨지나
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
