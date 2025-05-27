9e1d2f849be47e716881cfe42015cbdc6a1eb206
GUYoof <gom.nyusuk@gmail.com>
2025-05-27 10:22:09 +0900

    Test : 임시 스크립트 생성
    임시 스크립트 생성

---
+++ Assets/01.Scripts/02.UI/Point.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Point : MonoBehaviour
+{
+    public float curValue;      // 현재 값
+    public float startValue;    // 시작 값
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curValue = startValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/01.Scripts/04.Object/Item/BuffItem.cs
+++ Assets/01.Scripts/04.Object/Item/BuffItem.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BuffItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 버프 아이템의 효과를 플레이어에게 적용
+    /// </summary>
+    /// <param name="player">점프 강화 등 컨트롤 관련 효과를 적용할 플레이어</param>
+    /// <param name="point">코인 수량, 점수의 포인트를 적용할 컴포넌트</param>
+    /// <param name="data">적용할 아이템 데이터</param>
+    //public static void Apply(PlayerController player, ItemData data, Point point)
+    //{
+    //    // 소비형 아이템이 아니거나, 소비 효과 정보가 없으면 처리 중단
+    //    if (data.type != ItemType.Buff || data.Buff == null)
+    //        return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //    // 각 버프 효과를 순회하면서 적용
+    //    foreach (var buff in data.Buff)
+    //    {
+    //        switch (buff.type)
+    //        {
+    //            case BuffType.JumpBoost:
+    //                // 점프 강화 효과 적용 (지속 시간 기반)
+    //                player?.ApplyJumpBoost(buff.value);
+    //                break;
+
+    //            case BuffType.ScoringUp:
+    //                // 스코어 버프 효과 적용
+    //                point?.ApplyScoringUp(buff.value);
+    //                break;
+
+    //            case BuffType.CoinVolumeUp:
+    //                // 코인 버프 효과 적용
+    //                point?.ApplyCoinVolumeUp(buff.value);
+    //                break;
+    //        }
+    //    }
+    //}
 }
