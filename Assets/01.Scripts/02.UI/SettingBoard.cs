b367a11f740e21f5ef221e93e72b6f6610c2157b
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-27 20:31:19 +0900

    Chore : SettingBoard 스크립트 약 수정

--- Assets/01.Scripts/02.UI/SettingBoard.cs
+++ Assets/01.Scripts/02.UI/SettingBoard.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingBoard : UIBase
 {
-    
+    [SerializeField] private Slider bgmVolumeController;
+    [SerializeField] private Slider effectVolumeController;
+
+
 }
