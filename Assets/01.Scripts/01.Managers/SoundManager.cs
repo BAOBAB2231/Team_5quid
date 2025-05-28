c74a13b83b19d43412010bb38c1439e79a35c749
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-28 23:59:32 +0900

    Feat : SoundManager 생성 및 프리팹화
    +Prefabs 폴더에 Sound 폴더 추가

--- Assets/01.Scripts/01.Managers/SoundManager.cs
+++ Assets/01.Scripts/01.Managers/SoundManager.cs
@@ -1,11 +1,28 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
    private static SoundManager instance;
-   public static SoundManager Instance {get {return instance;}}
+
+    //Volume 범위 제한
+    [SerializeField][Range(0f, 1f)] private float effectVolume;
+    [SerializeField][Range(0f, 1f)] private float bgmVolume;
+
+    //effectVolume 프로퍼티화
+    public float EffectVolume
+    {
+        get; private set;
+    }
+
+    //bgmVolume 프로퍼티화
+    public float BgmVolume
+    {
+        get; private set;
+    }
+
+    public static SoundManager Instance {get {return instance;}}
     void Awake()
     {
         if (instance == null)
