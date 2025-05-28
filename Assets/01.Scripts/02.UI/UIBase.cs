cb8094d36a17b865aaf7b9cbe294a099f2438178
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-28 12:21:40 +0900

    Chore : UIBase 수정

--- Assets/01.Scripts/02.UI/UIBase.cs
+++ Assets/01.Scripts/02.UI/UIBase.cs
@@ -1,14 +1,6 @@
 ﻿using UnityEngine;
 public abstract class UIBase : MonoBehaviour
 {
-    private Canvas canvas;
-
-    //Canvas 있나 찾기
-    private void Start()
-    {
-        canvas = FindAnyObjectByType<Canvas>();
-    }
-
     public void Open()
     {
         gameObject.SetActive(true);
@@ -16,11 +8,5 @@ public abstract class UIBase : MonoBehaviour
     public void Close()
     {
         gameObject.SetActive(false);
-    }
-
-    //프리팹 불러올 때 Canvas의 자식으로 설정하기
-    public void SetCanvasChildren()
-    {
-        gameObject.transform.SetParent(canvas.transform, false);
     }
 }
