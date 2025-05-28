a545203eb4b1e05c6fdd811d5f4dac0e237f2cbe
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-28 10:08:04 +0900

    Feat : Canvas의 자식으로 프리팹 불러오는 메서드 추가

--- Assets/01.Scripts/02.UI/UIBase.cs
+++ Assets/01.Scripts/02.UI/UIBase.cs
@@ -1,6 +1,14 @@
 ﻿using UnityEngine;
 public abstract class UIBase : MonoBehaviour
 {
+    private Canvas canvas;
+
+    //Canvas 있나 찾기
+    private void Start()
+    {
+        canvas = FindAnyObjectByType<Canvas>();
+    }
+
     public void Open()
     {
         gameObject.SetActive(true);
@@ -10,8 +18,9 @@ public abstract class UIBase : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    //프리팹 불러올 때 Canvas의 자식으로 설정하기
     public void SetCanvasChildren()
     {
-        
+        gameObject.transform.SetParent(canvas.transform, false);
     }
 }
