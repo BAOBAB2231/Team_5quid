3159bd80c37688b5648fffac9be2fe500ec9e4d6
garamzui <forsave121@gmail.com>
2025-05-29 20:54:59 +0900

    Feat : 카메라 무빙 코드 수정

--- Assets/01.Scripts/03.Player/FollowCamera.cs
+++ Assets/01.Scripts/03.Player/FollowCamera.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-
     private Transform playerTransform; // 플레이어 프리팹의 트랜스폼
-   
+
     private Transform tf; //카메라 컨테이너의 트랜스폼 
+    private Transform cameraTf; //카메라의 트랜스폼
 
     private void Start()
     {
         playerTransform = GameObject.Find("Player").transform; //인스턴스 생성시 플레이어 캐싱 
         tf = GetComponent<Transform>();
+        cameraTf = FindObjectOfType<Camera>().transform;
     }
 
     void Update()
@@ -23,9 +24,27 @@ public class FollowCamera : MonoBehaviour
 
     void FollowPlayer()
     {
+        Vector3 currentPos = cameraTf.localPosition;
+        float targetY;
+
+        if (playerTransform.position.y >= 4f)
+        {
+            targetY = 9f;
+        }
+        else
+        {
+            targetY = 5f;
+        }
+
+
+        float smoothY = Mathf.Lerp(currentPos.y, targetY, 0.1f);
+
+        cameraTf.localPosition = new Vector3(0, smoothY, -5f);
+
+
         Vector3 playerPosition = new Vector3(0, 0, playerTransform.position.z); //z축만 추적 
-        
+
+
         tf.position = playerPosition;
     }
-
 }
--- Assets/01.Scripts/03.Player/Player.cs
+++ Assets/01.Scripts/03.Player/Player.cs
@@ -6,19 +6,17 @@ public abstract class Player : MonoBehaviour
     protected Transform tf;
     protected MyAnimation anim;
     protected SFXPlayer sfx;
+
     [Header("이동 관련 스탯")]
     //전방 이동속도
-    [SerializeField]
-    protected float runSpeed;
-
-    [SerializeField] protected float jumpForce;
+    [SerializeField] protected float runSpeed;
+   [SerializeField] protected float jumpForce;
     [SerializeField] protected float sideStepDistance;
     [SerializeField] protected float stepDuration;
     [SerializeField] protected float maxDistance;
     public LayerMask groundLayerMask;
 
-    
-  
+
     void Start()
     {
         anim = GetComponent<MyAnimation>();
