87109bc5d81485c222d32083758baee962c04acb
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-29 09:27:01 +0900

    Feat : SettingBoard에 SoundManager 값 동기화

--- Assets/01.Scripts/01.Managers/SoundManager.cs
+++ Assets/01.Scripts/01.Managers/SoundManager.cs
@@ -7,19 +7,21 @@ public class SoundManager : MonoBehaviour
    private static SoundManager instance;
 
     //Volume 범위 제한
-    [SerializeField][Range(0f, 1f)] private float effectVolume;
     [SerializeField][Range(0f, 1f)] private float bgmVolume;
-
-    //effectVolume 프로퍼티화
-    public float EffectVolume
-    {
-        get; private set;
-    }
+    [SerializeField][Range(0f, 1f)] private float effectVolume;
 
     //bgmVolume 프로퍼티화
     public float BgmVolume
     {
-        get; private set;
+        get => bgmVolume;
+        set => bgmVolume = value;
+    }
+
+    //effectVolume 프로퍼티화
+    public float EffectVolume
+    {
+        get => effectVolume;
+        set => effectVolume = value;
     }
 
     public static SoundManager Instance {get {return instance;}}
--- Assets/01.Scripts/02.UI/SettingBoard.cs
+++ Assets/01.Scripts/02.UI/SettingBoard.cs
@@ -8,5 +8,9 @@ public class SettingBoard : UIBase
     [SerializeField] private Slider bgmVolumeController;
     [SerializeField] private Slider effectVolumeController;
 
-
+    private void Update()
+    {
+        SoundManager.Instance.BgmVolume = bgmVolumeController.value;
+        SoundManager.Instance.EffectVolume = effectVolumeController.value;
+    }
 }
