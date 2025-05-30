92a8d2a38e9c3e1d136e8a818f98b8772afae8ec
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-05-30 21:29:10 +0900

    Fix : 마그넷 정상화

--- Assets/01.Scripts/04.Object/Item/Magnet.cs
+++ Assets/01.Scripts/04.Object/Item/Magnet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Magnet : MonoBehaviour
+public class Magnet : EffectItem
 {
     [SerializeField] private List<Coin> coins;
 
@@ -13,10 +13,10 @@ public class Magnet : MonoBehaviour
     /// </summary>
     /// <param name="player">효과를 적용할 대상 플레이어</param>
     /// <param name="effect">적용할 효과 타입 (사용 안 함)</param>
-    //public override void ApplyEffect(PlayerController player, EffectType effect)
-    //{
-    //    // 모든 코인을 순회하며 플레이어에게 이동
-    //    foreach (var coin in coins)
-    //        coin.MoveToPlayer(player.transform, speed);
-    //}
+    public override void ApplyEffect(PlayerController player, ItemDataEffect effectData)
+    {
+        // 모든 코인을 순회하며 플레이어에게 이동
+        foreach (var coin in coins)
+            coin.MoveToPlayer(player.transform, speed);
+    }
 }
