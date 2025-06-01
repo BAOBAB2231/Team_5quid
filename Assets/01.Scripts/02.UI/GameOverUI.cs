1523cafc232e9f9854febc466ad6d3e3fa26868d
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-01 19:42:24 +0900

    Feat: 게임오버 UI에 최종 점수 및 최고 점수 표시 기능 추가
    - 게임 종료 시 GameOverUI.Open()에서 ScoreManager로부터 점수를 받아와 표시
    - 최고 점수는 PlayerPrefs를 통해 저장 및 갱신되도록 처리
    - scoreText, bestText 필드를 GameOverUI에 추가하고 Inspector에서 연결

--- Assets/01.Scripts/02.UI/GameOverUI.cs
+++ Assets/01.Scripts/02.UI/GameOverUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,11 +10,35 @@ public class GameOverUI : UIBase
 {
     [SerializeField] private Button homeButton;
     [SerializeField] private Button retryButton;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text bestText;
 
     private void Awake()
     {
         homeButton.onClick.AddListener(OnClickHome);
         retryButton.onClick.AddListener(OnClickRetry);
+    }
+
+    public void UpdateScoreTexts(int score)
+    {
+        scoreText.text = score.ToString();
+
+        int best = PlayerPrefs.GetInt("BestScore", 0);
+        if (score > best)
+        {
+            best = score;
+            PlayerPrefs.SetInt("BestScore", best);
+        }
+
+        bestText.text = best.ToString();
+    }
+
+    public override void Open()
+    {
+        base.Open();
+
+        int finalScore = ScoreManager.Instance.GetScore();
+        UpdateScoreTexts(finalScore);
     }
 
     private void OnClickHome()
